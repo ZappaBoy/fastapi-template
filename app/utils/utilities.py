5b19ad020bc25b447ae5a1b51b811e4a54e88c20
Zappone Federico <45740621+ZappaBoy@users.noreply.github.com>
2022-03-06 12:35:50 +0100

    Update utilities.py

--- app/utils/utilities.py
+++ app/utils/utilities.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
 
+
 def get_interval_by_string(timeframe):
     # timeframes = ['1M', '1w', '3d', '1d', '12h', '8h', '6h', '4h', '2h', '1h', '30m', '15m', '5m', '3m', '1m']
     interval_digit = timeframe[0: len(timeframe) - 1]
