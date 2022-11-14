e09d0ba4136c2d8f6df78fd60d6676081e8b2dfc
zappaboy <federico_zappone@hotmail.it>
2022-11-14 11:10:35 +0100

    Added SimpleDict class

--- app/utils/utilities.py
+++ app/utils/utilities.py
@@ -1,6 +1,16 @@
 from datetime import datetime, timedelta
 
 
+class SimpleDict(dict):
+    """dot.notation access to dictionary attributes"""
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
+
+    def __getattr__(*args):
+        val = dict.get(*args)
+        return SimpleDict(val) if type(val) is dict else val
+
+
 def get_interval_by_string(timeframe):
     # timeframes = ['1M', '1w', '3d', '1d', '12h', '8h', '6h', '4h', '2h', '1h', '30m', '15m', '5m', '3m', '1m']
     interval_digit = timeframe[0: len(timeframe) - 1]
